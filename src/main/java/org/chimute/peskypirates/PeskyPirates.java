f93a55a6938e799e3dd667f292e36533d5b31d95
lunofe <git@lunofe.com>
2023-09-07 17:48:31 +0200

    we don't need no logging

--- src/main/java/org/chimute/peskypirates/PeskyPirates.java
+++ src/main/java/org/chimute/peskypirates/PeskyPirates.java
@@ -3,14 +3,11 @@ package org.chimute.peskypirates;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
 @Mod("peskypirates")
 public class PeskyPirates {
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public PeskyPirates() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
